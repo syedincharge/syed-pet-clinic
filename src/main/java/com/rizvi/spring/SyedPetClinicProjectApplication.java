13828f6d6da3503922e0701d4b8a47f5ff2563ce
syedincharge <rizvisyedba@gmail.com>
2022-03-13 00:40:52 -0600

    Initial commit

---
+++ src/main/java/com/rizvi/spring/SyedPetClinicProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rizvi.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyedPetClinicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyedPetClinicProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rizvi/spring/SyedPetClinicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rizvi.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyedPetClinicProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
