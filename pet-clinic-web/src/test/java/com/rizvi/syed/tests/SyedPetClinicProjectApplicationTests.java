f4d0cd1482ffd95f749f562048b0c8f865a01ab5
syedincharge <rizvisyedba@gmail.com>
2022-03-13 17:52:32 -0600

    updated poms

--- pet-clinic-web/src/test/java/com/rizvi/syed/tests/SyedPetClinicProjectApplicationTests.java
+++ pet-clinic-web/src/test/java/com/rizvi/syed/tests/SyedPetClinicProjectApplicationTests.java
@@ -1,9 +1,12 @@
 package com.rizvi.syed.tests;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class SyedPetClinicProjectApplicationTests {
 
 	@Test
