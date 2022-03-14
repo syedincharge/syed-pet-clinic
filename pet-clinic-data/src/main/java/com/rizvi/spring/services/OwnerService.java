422b5f72da6bd8feb46f550370eb56d8b9454858
syedincharge <rizvisyedba@gmail.com>
2022-03-14 00:12:38 -0600

    refracting to common crud interface. closes #11

--- pet-clinic-data/src/main/java/com/rizvi/spring/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/rizvi/spring/services/OwnerService.java
@@ -2,13 +2,9 @@ package com.rizvi.spring.services;
 
 import com.rizvi.spring.model.Owner;
 
-import java.util.Set;
 
-public interface OwnerService {
-
+public interface OwnerService extends CrudService<Owner, Long>{
 
      Owner findByLastName(String lastName);
-     Owner findById(Long id);
-     Owner save(Owner owner);
-     Set<Owner> findAll();
+
 }
