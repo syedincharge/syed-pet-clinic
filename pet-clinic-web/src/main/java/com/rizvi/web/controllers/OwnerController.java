dad71e52a0bb1eaef15f0ddd4b69759bc3efd9e0
syedincharge <rizvisyedba@gmail.com>
2022-03-14 13:12:38 -0600

    Added Owners Controllers and Owners index page. closes #13

---
+++ pet-clinic-web/src/main/java/com/rizvi/web/controllers/OwnerController.java
@@ -0,0 +1,17 @@
+package com.rizvi.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+
+    @RequestMapping({"", "/index", "/index.html"})
+    public String listOwners(){
+
+        return "owners/index";
+    }
+
+}
