b93ec651c3ba1d87f0dbae754224d0dd86db953f
syedincharge <rizvisyedba@gmail.com>
2022-03-16 18:46:03 -0600

    Add banner, closes #3

--- pet-clinic-data/src/main/java/com/rizvi/spring/model/Visit.java
+++ pet-clinic-data/src/main/java/com/rizvi/spring/model/Visit.java
@@ -9,6 +9,23 @@ public class Visit extends BaseEntity {
     private String description;
     private Pet pet;
 
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
     public LocalDate getDate() {
         return date;
     }
