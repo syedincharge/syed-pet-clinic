62bd584df84364b79c4ac09310268d077c98ea90
syedincharge <rizvisyedba@gmail.com>
2022-03-17 18:37:08 -0600

    Menu Links updated temporary. closes #46

--- pet-clinic-web/src/main/java/com/rizvi/spring/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/rizvi/spring/controllers/OwnerController.java
@@ -22,5 +22,11 @@ public class OwnerController {
 
         return "owners/index";
     }
+    @RequestMapping({"","/find"})
+    public String findOwners(){
+
+        return "notimplemented";
+    }
+
 
 }
