23cd6d606f79cd46d524ecc7e308413766b980a0
syedincharge <rizvisyedba@gmail.com>
2022-03-18 10:31:42 -0600

    Convert Entity classes to Jpa persistance annotation. closes #37

--- pet-clinic-data/src/main/java/com/rizvi/spring/model/Person.java
+++ pet-clinic-data/src/main/java/com/rizvi/spring/model/Person.java
@@ -1,11 +1,15 @@
 package com.rizvi.spring.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity{
 
-   // @Column(name = "first_name")
+   @Column(name = "first_name")
     private String firstName;
 
-   // @Column(name = "last_name")
+    @Column(name = "last_name")
     private String lastName;
 
 
