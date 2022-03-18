9658540df38ed15011930c0fb9d14c5338899868
syedincharge <rizvisyedba@gmail.com>
2022-03-18 11:10:25 -0600

    Updated Vet and Pet Entities to Jpa persistance annotation. closes #39

--- pet-clinic-data/src/main/java/com/rizvi/spring/model/Pet.java
+++ pet-clinic-data/src/main/java/com/rizvi/spring/model/Pet.java
@@ -1,10 +1,9 @@
 package com.rizvi.spring.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -20,6 +19,9 @@ public class Pet extends BaseEntity{
     private Owner owner;
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -51,5 +53,13 @@ public class Pet extends BaseEntity{
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
